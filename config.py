d3a7b3f2213b803d7bcca1ad1a8a9edd8f33904f
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-09-30 08:52:58 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+COOKIES_PATH = os.getenv("COOKIES_PATH", "cookies.txt")
+DOWNLOADS_DIR = os.getenv("DOWNLOADS_DIR", "downloads")
